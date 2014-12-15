b80c4bd1563e73eaa010a00c3a7858767bd933c0
Sander Siim <sander.siim1@gmail.com>
2014-12-15 16:31:31 +0200

    Fixed some bugs. Evaluation now gives intuitively very reasonable results. Need to fix a corner case for when 3 points or on a line and can't draw circle through them.

--- solver/city_order_finder.c
+++ solver/city_order_finder.c
@@ -30,19 +30,23 @@ double angle(Vector& a, Vector& b) {
     return acos(dotProduct(a, b)/(magnitude(a)*magnitude(b)));
 }
 
-// from http://mathforum.org/library/drmath/view/54323.html
-Vector circleCenterPoint(Vector& a, Vector& b, Vector& c) {
-    float temp = b.x*b.x+b.y*b.y;
-    double ab = (a.x*a.x + a.y*a.y - temp)/2.0d;
-    double bc = (temp - c.x*c.x - c.y*c.y)/2.0d;
-    double det = (a.x-b.x)*(b.y-c.y)-(b.x-c.x)*(a.y-b.y);
-    if (abs(det) < 1.0e-6) {
-        assert(false);
-    }
-    det = 1/det;
-    double center_x = (ab*(b.y-c.y)-bc*(a.y-b.y))*det;
-    double center_y = ((a.x-b.x)*bc-(b.x-c.x)*ab)*det;
-    return Vector(center_x, center_y);
+// from http://forums.techarena.in/software-development/1415154.htm
+Vector circleCenterPoint(Vector& p, Vector& q, Vector& r) {
+//    cout << p << ", " << q << ", " << r << endl;
+    double p1, y11, yd1, pd1, p2, y2, yd2, pd2, ox, oy;
+    p1 = (q.x + p.x) / 2;
+    y11 = (q.y + p.y) / 2;
+    yd1 = q.x - p.x;
+    pd1 = -(q.y - p.y);
+    //***********************
+    p2 = (r.x + q.x) / 2;
+    y2 = (r.y + q.y) / 2;
+    yd2 = r.x - q.x;
+    pd2 = -(r.y - q.y);
+    //****************************
+    ox = (y11 * pd1 * pd2 + p2 * pd1 * yd2 - p1 * yd1 * pd2 - y2 * pd1 * pd2)/ (pd1 * yd2 - yd1 * pd2);
+    oy = (ox - p1) * yd1 / pd1 + y11;
+    return Vector(ox, oy);
 }
 
 double evaluateCityOrder(CityOrder& order) {
@@ -60,13 +64,17 @@ double evaluateCityOrder(CityOrder& order) {
 
         circleCenter = circleCenterPoint(currentPos, pos1, pos2);
         radius = distance(currentPos, circleCenter);
+//        cout << "center: " << circleCenter << endl;
+//        cout << "radius: " << radius << endl;
         //speed = sqrt(radius);
+
+        //TODO: Handle situation where points are on the same line
 
         vec_a = Vector(currentPos.x - circleCenter.x, currentPos.y - circleCenter.y);
         vec_b = Vector(pos1.x - circleCenter.x, pos1.y - circleCenter.y);
         vec_c = Vector(pos2.x - circleCenter.x, pos2.y - circleCenter.y);
-        vec_up = Vector(0, radius);
-        vec_down = Vector(0, -radius);
+        vec_up = Vector(0, -radius);
+        vec_down = Vector(0, radius);
 
         trajectory_angle = angle(vec_a, vec_b);
 
@@ -120,6 +128,7 @@ double evaluateCityOrder(CityOrder& order) {
 
         //arc_length = trajectory_angle*radius;
         totalTime += trajectory_angle*sqrt(radius);
+//        cout << "Added trajectory time: " << trajectory_angle*sqrt(radius) << endl;
 
         if (clockWise && currentPos.y >= circleCenter.y) {
             newSpeed = Vector(1.0d, -(vec_a.x/vec_a.y));
@@ -135,6 +144,7 @@ double evaluateCityOrder(CityOrder& order) {
 
         Vector differenceVector = Vector(newSpeed.x-previousSpeed.x, newSpeed.y-previousSpeed.y);
         totalTime += magnitude(differenceVector);
+//        cout << "Added velocity change time: " << magnitude(differenceVector) << endl;
 
         if (clockWise && pos1.y >= circleCenter.y) {
             previousSpeed = Vector(1.0d, -(vec_b.x/vec_b.y));
@@ -148,7 +158,7 @@ double evaluateCityOrder(CityOrder& order) {
 
         previousSpeed = previousSpeed*(sqrt(radius)/magnitude(previousSpeed));
 
-        if (i == order.size()-1) {
+        if (i == order.size()-2) {
             double bc_angle;
             if (clockWise) {
                 if (pos1.x >= circleCenter.x && pos2.x >= circleCenter.x) {
@@ -158,6 +168,10 @@ double evaluateCityOrder(CityOrder& order) {
                     assert(pos1.y > pos2.y);
                     bc_angle = angle(vec_b, vec_c);
                 } else if (pos1.x >= circleCenter.x && pos2.x < circleCenter.x) {
+//                    cout << "Hello" << endl;
+//                    cout << vec_b << ", " << vec_down << endl;
+//                    cout << angle(vec_b, vec_down) << endl;
+//                    cout << angle(vec_c, vec_down) << endl;
                     bc_angle = angle(vec_b, vec_down) + angle(vec_c, vec_down);
                 } else {
                     bc_angle = angle(vec_b, vec_up) + angle(vec_c, vec_up);
@@ -177,6 +191,10 @@ double evaluateCityOrder(CityOrder& order) {
             }
 
             totalTime += bc_angle*sqrt(radius);
+//            cout << "Added last arc time: " <<  bc_angle*sqrt(radius) << endl;
+
+        } else {
+            currentPos = pos1;
         }
     }
 
@@ -244,6 +262,19 @@ int main(int argc, char**argv) {
     cities = {A,D,C,B};
 
     cout << "A,D,C,B = " << evaluateCityOrder(cities) << endl;
+
+
+    //Test 3 points on line
+    /*A = Vector(100,140);
+    B = Vector(185,141);
+    C = Vector(230,141);
+    D = Vector(240,141);
+
+    cities = {A,B,D,C};
+
+    cout << "A,B,D,C = " << evaluateCityOrder(cities) << endl;*/
+
+
 
 
 }
