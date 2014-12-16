8ce661178f49f97239757c5eb48a162d117b1eef
Karl Tarbe <karulont@gmail.com>
2014-12-16 02:17:16 +0200

    GA on force vectors

--- solver/solver.cpp
+++ solver/solver.cpp
@@ -8,6 +8,7 @@
 #include <functional>
 #include <algorithm>
 #include <queue>
+#include <cassert>
 #include "vector.h"
 #include "util.h"
 
@@ -17,15 +18,18 @@ mt19937 ranEng;
 
 // Globals here
 
-const size_t populationSize = 50;
-const size_t numberOfParents = 3;
-const size_t numberOfNew = 80;
+const size_t populationSize = 25;
+const size_t numberOfParents = 5;
 
 struct Trajectory {
     double fitness;
     int size;
     vector<Action> acts;
 };
+
+
+double globalMax;
+Trajectory globalBest;
 
 vector<Vector> cities;
 vector<Trajectory> population;
@@ -35,6 +39,8 @@ normal_distribution<double> normalDist(0,1);
 uniform_int_distribution<> geneDist(0,4);
 uniform_int_distribution<> disMating(0, numberOfParents - 1);
 uniform_int_distribution<> disPopulation(0, populationSize - 1);
+binomial_distribution<> swapDist(40, 0.3);
+bernoulli_distribution flipDist(0.05);
 
 void mutateSwap(Trajectory & t) {
     uniform_int_distribution<> d(2, t.size);
@@ -82,6 +88,7 @@ void fitness(Trajectory & t) {
 
     t.size = t.acts.size();
     for (auto & a : t.acts) {
+        applyAction(pos, speed, a);
         if (touched(pos, cities[cityIndex])) {
             ++cityIndex;
             if (cityIndex == cities.size()) {
@@ -90,7 +97,6 @@ void fitness(Trajectory & t) {
             }
         }
         ++steps;
-        applyAction(pos, speed, a);
     }
 
     double e = 0;
@@ -98,7 +104,8 @@ void fitness(Trajectory & t) {
     if (cityIndex != cities.size()) {
         e = 1.0 / sqrt(distSqr(pos, cities[cityIndex]));
     }
-    double fitness = cityIndex + e + ( 1.0 / t.size);
+    double fitness = (cityIndex/1.0) + e + ( 1.0 / t.size);
+    assert(fitness > 0 && fitness < cityIndex + 1);
     t.fitness = fitness;
 }
 
@@ -125,17 +132,20 @@ Trajectory & tournamentSelect() {
 
 void naturalSelection(vector<Trajectory> & parents)
 {
-    parents.clear();
-    while (parents.size() < numberOfParents) {
-        parents.push_back(tournamentSelect());
+    for (int i = 0; i < numberOfParents; ++i) {
+        parents[i] = tournamentSelect();
     }
 }
 
 void reproduce(vector<Trajectory> & matingPool) {
     for (int i = 0; i < populationSize; ++i) {
         population[i] = matingPool[disMating(ranEng)];
-        mutateSwap(population[i]);
-        //mutateFlip(population[i]);
+        int swaps = swapDist(ranEng);
+        while (swaps--) {
+            mutateSwap(population[i]);
+        }
+        if (flipDist(ranEng))
+            mutateFlip(population[i]);
     }
 }
 
@@ -166,8 +176,8 @@ void printResult(vector<Action> & actions) {
 }
 
 Trajectory & findBestSoltion() {
-    double bestfit = 0;
-    Trajectory * best = nullptr;
+    double bestfit = globalBest.fitness;
+    Trajectory * best = &globalBest;
     double fit;
     for (auto & p : population) {
         if (p.fitness > bestfit) {
@@ -178,22 +188,53 @@ Trajectory & findBestSoltion() {
     return *best;
 }
 
+inline void calcStatistics(const vector<Trajectory> & pop, double & avg,
+                           double & max)
+{
+    max = 0;
+    double sum = 0;
+    for (const auto & p : pop) {
+        sum += p.fitness;
+        if (p.fitness > max) {
+            max = p.fitness;
+            if (max > globalMax) {
+                globalBest = p;
+                globalMax = max;
+            }
+        }
+    }
+    avg = sum / pop.size();
+}
+
 int main(int argc, char**argv) {
 
     readInput(argv[1]);
 
     initPopulation();
+    evaluateAll();
     vector<Trajectory> parents;
     parents.resize(numberOfParents);
-    for (size_t i = 0; i < 1000; ++i) {
-        evaluateAll();
+    double avg, max;
+    bool printStat = false;
+    for (size_t i = 0; i < 200000; ++i) {
+        printStat = !(i % 0xff);
+        if (printStat) {
+            calcStatistics(population, avg, max);
+            cout << "Pop: avg: " << avg << "\tmax: " << max << "\t";
+        }
         naturalSelection(parents);
+        if (printStat) {
+            calcStatistics(parents, avg, max);
+            cout << "Par: avg: " << avg << "\tmax: " << max << endl;
+        }
         reproduce(parents);
+        evaluateAll();
     }
 
     Trajectory & p = findBestSoltion();
     cout << p.fitness << endl;
     cout << p.size << endl;
+    p.acts.resize(p.size);
     printResult(p.acts);
 
     return 0;
